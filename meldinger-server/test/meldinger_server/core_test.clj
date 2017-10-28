7a0f1dc83d0ce63b4d4360f222679fa6e4208121
Antanas Bastys <antanasb@wix.com>
2017-10-28 15:55:35 +0300

    generate server project

---
+++ meldinger-server/test/meldinger_server/core_test.clj
@@ -0,0 +1,7 @@
+(ns meldinger-server.core-test
+  (:require [clojure.test :refer :all]
+            [meldinger-server.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
